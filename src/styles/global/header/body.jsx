6e4cff4e551217e6e10504ee4effbe2e08263a09
양민석 <kurooru98@gmail.com>
2024-10-06 16:15:24 +0900

    header advise

--- src/styles/global/header/body.jsx
+++ src/styles/global/header/body.jsx
@@ -1,10 +1,23 @@
 import { BodyDiv } from "./style";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Body() {
+    const navigate = useNavigate();
+    const location = useLocation(); // 현재 위치 가져오기
+
+    const handleOnclickBtn = (e) => {
+        const order = e.target.id;
+        if (order === 'logo' && location.pathname !== '/') { // 메인 페이지가 아닐 때만 실행
+            const ans = window.confirm('本当に戻りますか？');
+            if (ans) {
+                navigate('/');
+            }
+        }
+    }
 
     return (
         <BodyDiv>
-            <div></div>
+            <div id="logo" onClick={handleOnclickBtn}></div>
         </BodyDiv>
     );
 }
